e073fb3c15e8b914c6b1ec290d1cb81be4fc152a
Calvin Chen <calvinchen0123@gmail.com>
2025-08-03 16:05:42 -0600

    add example file

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,5 +1,5 @@
-# from typing import Type
+# # from typing import Type
 
-def add_numbers(a, b, c):
-    """Return the sum of a, b, c"""
-    return sum([a, b, c])
+# def add_numbers(a, b, c):
+#     """Return the sum of a, b, c"""
+#     return sum([a, b, c])
